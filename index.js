99fc9f36936da1f657a06658d6989eef24bd8f0b
harshal patel <41120010+harshalpat@users.noreply.github.com>
2021-03-22 18:39:28 +0100

    added route in index.js file

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const http = require('http');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const { read } = require('fs');
+const dishRouter = require('./routes/dishRouter');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -10,6 +11,8 @@ const port = 3000;
 const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
+
+app.use('/dishes', dishRouter);
 
 app.use(express.static(__dirname + '/public'))
 
